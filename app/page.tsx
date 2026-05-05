6109f85d109a4d8d6bc5ddc3ed345f726abea8bc
Global Marks <globalmarksdeveloper@gmail.com>
2026-05-05 10:15:27 +0500

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import Logo from "@/public/gmpl-logo/gmpl-third-logo.png";
+import Logo from "@/public/gmpl-logo/gmpl-360-Logo.png";
 
 const features = [
   {
